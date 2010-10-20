458ee95dd05f9e0310daf15f3c81a5a5e3f23c33
centralniak <p.kilczuk@stylesheet.pl>
2010-10-20 16:59:17 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='django_centralniak_slugfield',
-    version='0.1.1',
+    name='django-centralniak-slugfield',
+    version='0.1.2',
     author='Piotr Kilczuk',
     author_email='p.kilczuk@neumea.pl',
     url='http://github.com/centralniak',
     description = 'Provides more handy support for slugs in models',
-    packages = ['django_centralniak_slugfield',],
+    packages=find_packages(),
+    provides=['django_centralniak_slugfield'],
+    classifiers=[
+        'Framework :: Django',
+        #'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
